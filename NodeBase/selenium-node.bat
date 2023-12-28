37597c31791867a8addf1d154d61f8da4369050c
Nikhil Agarwal <nikhil.ag@browserstack.com>
2023-12-28 17:27:18 +0530

    fix screen resorder

--- NodeBase/selenium-node.bat
+++ NodeBase/selenium-node.bat
@@ -1,4 +1,6 @@
 rem Calling node config generation script
 call C:\opt\generate_node_config.bat
 
+start "NodeApp" node C:\high-scale-testing\screen_recorder\index.js
+
 java %SE_JAVA_OPTS% -Dwebdriver.http.factory=jdk-http-client -jar C:\opt\selenium\selenium-server.jar --ext C:\opt\selenium\selenium-http-jdk-client.jar node --bind-host false --config %CONFIG_FILE% --log-level FINE --log C:\opt\selenium\assets\selenium.log
